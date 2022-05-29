5c1a2e27996d9dbadce4ea6946b83840a4beb2a2
superninja0119 <wstar3233@gmail.com>
2022-05-30 02:57:43 +0900

    feat : mobile responsive

--- src/views/Main/SubMenu/index.tsx
+++ src/views/Main/SubMenu/index.tsx
@@ -4,19 +4,19 @@ const SubMenu = () => {
   return (
     <Top>
       <MenuItem>
-        <MenuImg src="img/buildyourdeck.png" alt="" />
+        <MenuImg src="img/buildyourdeck.png" alt="" draggable={false} />
         <Text>Build your deck</Text>
       </MenuItem>
       <MenuItem>
-        <MenuImg src="img/exploredistinct.png" alt="" />
+        <MenuImg src="img/exploredistinct.png" alt="" draggable={false} />
         <Text>Explore distinct areas</Text>
       </MenuItem>
       <MenuItem>
-        <MenuImg src="img/fight.png" alt="" />
+        <MenuImg src="img/fight.png" alt="" draggable={false} />
         <Text>Fight fearsome enemies</Text>
       </MenuItem>
       <MenuItem>
-        <MenuImg src="img/defeat.png" alt="" />
+        <MenuImg src="img/defeat.png" alt="" draggable={false} />
         <Text>Defeat terrifying bosses</Text>
       </MenuItem>
     </Top>
